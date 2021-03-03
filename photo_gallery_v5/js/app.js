bdd2b278dd6ab3980dd7507ec0266437a018e4e7
gerlinp <gerlin.pierrelouis@gmail.com>
2021-03-02 21:18:54 -0500

    final update

--- photo_gallery_v5/js/app.js
+++ photo_gallery_v5/js/app.js
@@ -1,3 +1,24 @@
-baguetteBox.run('.grid', {
-    // Custom options
+
+baguetteBox.run('.grid');
+
+
+
+
+// ------------ Search and Filter-------------//
+// code based on "JavaScript DOM Tutorial #16" from https://youtu.be/3NG8zy0ywIk
+const list = document.querySelector('#image-list ul');
+const forms = document.forms;
+const searchBar = forms['search-images'].querySelector('input');
+
+searchBar.addEventListener('keyup', (e) => {
+  const term = e.target.value.toLowerCase();
+  const images = list.getElementsByTagName('li');
+  Array.from(images).forEach((image) => {
+    const title = image.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(e.target.value) != -1){
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
 });
